80942ca8825aa5d8948c1d34c07bc26a15970bb2
Atticus T <theresurgence2@proton.me>
2022-09-12 10:22:14 +0800

    Add ui

---
+++ src/canopto/ui/messages.py
@@ -0,0 +1,96 @@
+from prompt_toolkit import print_formatted_text, HTML
+from prompt_toolkit.formatted_text import FormattedText
+from prompt_toolkit.styles import Style
+
+from core.config import get_download_dir, is_download_dir_valid
+
+STYLE = Style.from_dict({
+    'error': 'fg:#ff0066',
+    'warning': 'fg:ansiyellow',
+    'info': 'fg:ansibrightblue',
+    'time': 'fg:orange',
+    'underline': 'underline',
+    'ub': 'underline bold',
+    'cyan': 'fg: ansicyan',
+    'strike': 'strike',
+})
+
+
+def print_request_error():
+    msg = FormattedText([
+        ('class:error', 'Connection Error. Please check your internet connection!')
+    ])
+    print_formatted_text(msg, style=STYLE)
+
+
+def print_authentication_error(status_code):
+    msg = FormattedText([
+        ('class:error', f'Error {status_code}. Please check your username and password!')
+    ])
+    print_formatted_text(msg, style=STYLE)
+
+
+def print_folder_warning():
+    msg = FormattedText([
+        ('class:warning', 'You have not chosen a folder to store your course files and videos.')
+    ])
+    print_formatted_text(msg, style=STYLE)
+    print()
+
+
+async def print_folder_notification():
+    if await is_download_dir_valid():
+        msg = FormattedText([
+            ('', 'Folder to store files and videos: '),
+            ('class:ansicyan', f'{get_download_dir()}'),
+        ])
+    else:
+        msg = FormattedText([
+            ('class:warning', 'You cannot download any files or videos until you choose a folder to store them.')
+        ])
+    print_formatted_text(msg, style=STYLE)
+    print_formatted_text("")
+
+
+def print_download_time(file_or_video, time_taken):
+    msg = FormattedText([
+        ('class:time', f'Finished downloading {file_or_video} in {time_taken}s.')
+    ])
+    print_formatted_text(msg, style=STYLE)
+
+
+def print_download_time(file_or_video, time_taken):
+    msg = FormattedText([
+        ('class:time', f'Finished downloading {file_or_video} in {time_taken}s.\n')
+    ])
+    print_formatted_text(msg, style=STYLE)
+
+
+def print_list_courses():
+    msg = FormattedText([
+        ('class:ub', 'You are taking the following courses:')
+    ])
+    print_formatted_text(msg, style=STYLE)
+
+
+def print_faq():
+    faq_text = HTML("""
+<aaa bg='seagreen'><bold>FAQ</bold></aaa>
+<underline>1. How are my login credentials stored?</underline>
+Your username and password is stored locally and securely on your computer using the python keyring library.
+For more information please check out: https://pypi.org/project/keyring/
+Credentials are only used for authenticating with NUS Canvas servers.
+
+<underline>2. Video downloads seem to take very long!</underline>
+Videos can be large, and can easily be a few hundred MBs each. Please wait patiently.
+
+<underline>3. If my download is interrupted, can I resume it?</underline>
+Yes, you can end downloads abruptly and resume them at any time.
+Partially downloaded files have a ".part" file extension, which will be removed
+once the download is fully complete.
+
+<underline>4. Will my annotated files be overwritten?</underline>
+No, if the file has been full downloaded to your computer (no more ".part" extension), it will be ignored.
+
+""")
+    print_formatted_text(faq_text)
---
+++ src/canopto/ui/tui.py
@@ -0,0 +1,128 @@
+from __future__ import unicode_literals
+
+from prompt_toolkit import PromptSession
+from prompt_toolkit import print_formatted_text, HTML
+from prompt_toolkit.key_binding.key_bindings import KeyBindings
+from prompt_toolkit.patch_stdout import patch_stdout
+from prompt_toolkit.shortcuts import create_confirm_session
+from prompt_toolkit.validation import Validator, ValidationError
+
+from classes.menuoption import MenuOption
+from core.config import is_download_dir_valid
+from ui.messages import print_folder_notification
+
+
+class BlankValidatorNoMessage(Validator):
+    def validate(self, document):
+        text = document.text
+
+        if len(text) == 0:
+            raise ValidationError()
+
+
+class BlankValidatorWithMessage(Validator):
+    def validate(self, document):
+        text = document.text
+
+        if len(text) == 0:
+            raise ValidationError(message='Field cannot be blank!', cursor_position=0)
+
+
+class OptionValidator(BlankValidatorNoMessage):
+    def validate(self, document):
+        super().validate(document)
+        text = document.text
+
+        if len(text) > 1 or not text.isdigit() or int(text) < 1 or int(text) > 5:
+            raise ValidationError(message='Only numbers from 1-5 are allowed', cursor_position=0)
+
+
+async def get_credentials_prompt():
+    session = PromptSession()
+    with patch_stdout():
+        username = await session.prompt_async('Enter NUSNET ID (exxxxxxx): ', validator=BlankValidatorWithMessage())
+        password = await session.prompt_async('Password: ', is_password=True, validator=BlankValidatorWithMessage())
+    return username, password
+
+
+async def ask_save_credentials_prompt():
+    session = create_confirm_session("Do you want to save username and password on your computer?",
+                                     suffix=" Press (y)es/(n)o: ")
+    return await session.prompt_async(validator=BlankValidatorNoMessage())
+
+
+bindings = KeyBindings()
+
+
+@bindings.add(str(1))
+def exit_with_value(event):
+    event.app.exit(result=MenuOption.DL_ALL)
+
+
+@bindings.add(str(2))
+def exit_with_value(event):
+    event.app.exit(result=MenuOption.DL_FILES)
+
+
+@bindings.add(str(3))
+def exit_with_value(event):
+    event.app.exit(result=MenuOption.DL_VIDEOS)
+
+
+@bindings.add(str(4))
+def exit_with_value(event):
+    event.app.exit(result=MenuOption.SEL_FOLDER)
+
+
+@bindings.add(str(5))
+def exit_with_value(event):
+    event.app.exit(result=MenuOption.FAQ)
+
+
+@bindings.add(str(6))
+def exit_with_value(event):
+    event.app.exit(result=MenuOption.QUIT)
+
+
+half_bindings = KeyBindings()
+
+
+@half_bindings.add(str(4))
+def exit_with_value(event):
+    event.app.exit(result=MenuOption.SEL_FOLDER)
+
+
+@half_bindings.add(str(5))
+def exit_with_value(event):
+    event.app.exit(result=MenuOption.FAQ)
+
+
+@half_bindings.add(str(6))
+def exit_with_value(event):
+    event.app.exit(result=MenuOption.QUIT)
+
+
+async def select_option_prompt(courses):
+    with patch_stdout():
+        courses.list_courses()
+        await print_folder_notification()
+        print_formatted_text(HTML("<aaa bg='ansiblue' ><b>Select option number:</b></aaa>"))
+
+        if await is_download_dir_valid():
+            session = PromptSession(key_bindings=bindings, validator=OptionValidator())
+            print_formatted_text(HTML("<aaa >1. Download all files + videos</aaa>"))
+            print_formatted_text(HTML("<aaa >2. Download all files </aaa>"))
+            print_formatted_text(HTML("<aaa >3. Download all videos</aaa>"))
+        else:
+            session = PromptSession(key_bindings=half_bindings, validator=OptionValidator())
+            print_formatted_text(
+                HTML("<aaa ><strike fg='ansibrightblack'>1. Download all files + videos</strike></aaa>"))
+            print_formatted_text(HTML("<aaa ><strike fg='ansibrightblack'>2. Download all files</strike></aaa>"))
+            print_formatted_text(HTML("<aaa ><strike fg='ansibrightblack'>3. Download all videos</strike></aaa>"))
+
+        print_formatted_text(HTML("<aaa >4. Choose download folder</aaa>"))
+        print_formatted_text(HTML("<aaa >5. FAQ</aaa>"))
+        print_formatted_text(HTML("<aaa >6. Quit</aaa>\n"))
+        print_formatted_text(HTML("<aaa fg='yellow' >Ctrl-C to exit anytime</aaa>"))
+        result = await session.prompt_async(editing_mode=False)
+        return result
