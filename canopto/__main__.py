c63d51948f2401d29de870338dbea17f1d5d0d90
Atticus T <theresurgence2@proton.me>
2022-11-21 20:37:45 +0800

    test

--- canopto/__main__.py
+++ canopto/__main__.py
@@ -1,10 +1,10 @@
 #!/bin/python3
 import asyncio
 
-from classes.courses import Courses
-from core.authenticate import authentication_loop
-from core.config import parse_config_file
-from core.menu import select_menu
+from canopto.classes.courses import Courses
+from canopto.core.authenticate import authentication_loop
+from canopto.core.config import parse_config_file
+from canopto.core.menu import select_menu
 from core.web import close_aclient
 
 
